56ab2134a29b544999560cc51f322144b73da5a8
jrcast <jrcast@users.noreply.github.com>
2019-05-22 21:31:36 -0700

    improved packaging of original version. Improved logging

--- pyssher/pyssher.py
+++ pyssher/pyssher.py
@@ -2,19 +2,12 @@ import argparse
 import asyncio
 import asyncssh
 import logging
+import os
+import re
 import signal
 import sys
 
-log_level = logging.DEBUG
-server_command = 'while true; do echo $HOSTNAME && sleep 1; done'
-servers = [{"hostname": "localhost",
-            "port": 10000,
-            "username": "root",
-            "client_key": "id_rsa"},
-           {"hostname": "localhost",
-            "port": 10001,
-            "username": "root",
-            "client_key": "id_rsa"}]
+log_level = logging.INFO
 
 
 class MySSHClientSession(asyncssh.SSHClientSession):
@@ -33,7 +26,6 @@ class MySSHClientSession(asyncssh.SSHClientSession):
 
     def data_received(self, data, datatype):
         self.logger.info(data.replace("\n", ""))
-#         print(data, end='')
 
     def connection_lost(self, exc):
         if exc:
@@ -41,16 +33,21 @@ class MySSHClientSession(asyncssh.SSHClientSession):
 
 
 class Pyssher():
+    '''
+    @param clients: dictironary with the client information. {hostname:, port:, username:}
+    '''
 
     def __init__(self, clients):
         self.logger = logging.getLogger("pyssher")
         self.logger.setLevel(log_level)
-        handler = logging.StreamHandler()
-        self.logger.addHandler(handler)
+        logformat = logging.Formatter('%(levelname)-8s %(message)s')
+        if not len(self.logger.handlers):
+            handler = logging.StreamHandler()
+            handler.setFormatter(logformat)
+            self.logger.addHandler(handler)
 
         self.connections = []
         self.clients = clients
-        #select which signals will be captured. These signals will be forwarded to remote clients once received
         self.signals_to_capture = {signal.SIGTERM: "TERM",
                                    signal.SIGINT: "INT"}
         for registered_signal in self.signals_to_capture:
@@ -66,34 +63,77 @@ class Pyssher():
                 self.logger.error("Error sending signal {} to {}".format(sig, conn))
                 exit_code = -1
 
-        self.logger.debug("Goodbye cruel world. I am now dead")
+        self.logger.info("Goodbye cruel world. me now dead")
         sys.exit(exit_code)
 
-    async def run_command_in_client(self, host, port, username, client_key, command):
-        async with asyncssh.connect(host,
-                                    port = port,
-                                    username = username,
-                                    client_keys = client_key,
-    #                                 known_hosts = asyncssh.read_known_hosts("host_keys")) as conn:
-                                    known_hosts = None) as conn:
-            #         return await conn.create_session(MySSHClientSession, command)
-            clientSession = MySSHClientSession("{}@{}:{}".format(username, host, port))
-            chan, _session = await conn.create_session(clientSession, command)
-            self.connections.append(conn)
-            await chan.wait_closed()
+    async def run_command_in_client(self, host, port, username, command, client_keys):
+        try:
+            async with asyncssh.connect(host,
+                                        port = port,
+                                        username = username,
+                                        client_keys = client_keys,
+                                        #known_hosts = asyncssh.read_known_hosts("host_keys")) as conn:
+                                        known_hosts = None) as conn:
 
-    async def run_command_in_all_clients(self, command):
+                clientSession = MySSHClientSession("{}@{}:{}".format(username, host, port))
+                chan, _session = await conn.create_session(clientSession, command)
+                self.connections.append(conn)
+                await chan.wait_closed()
+
+        except Exception as e:
+            self.logger.error("When attempting to connect to {}@{}:{} got: {}".format(username, host, port, e))
+
+    async def run_command_in_all_clients(self, command, keypath):
+        paths = [os.path.join(keypath, f) for f in os.listdir(keypath)]
         sessions = [self.run_command_in_client(host=client["hostname"],
-                                                    port=client["port"],
-                                                    username=client["username"],
-                                                    client_key=client["client_key"],
-                                                    command=command) for client in self.clients]
+                                               port=client["port"],
+                                               username=client["username"],
+                                               client_keys=paths,
+                                               command=command) for client in self.clients]
+
         await asyncio.gather(*sessions, return_exceptions=True)
 
 
 def pyssher():
+    global log_level
+    logger = logging.getLogger("main")
+    logformat = logging.Formatter('%(levelname)-8s %(message)s')
+    handler = logging.StreamHandler()
+    handler.setFormatter(logformat)
+    logger.addHandler(handler)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--debug", dest="debug", action="store_true", help="enable debug mode")
+    parser.add_argument("-s", "--server", dest="servers", action="append", nargs="*",
+                        help="input each server in the following format user@hostname:port")
+    parser.add_argument("-c", "--command", dest="command", help="Shell command you want to execute in given servers")
+    parser.add_argument("-k", "--keypath", dest="keypath", default= "/pyssher/keys",
+                        help="Path to the directory containing ssh keys to use. Defaults to /pyssher/keys")
+
+    args = parser.parse_args()
+
+    if args.debug:
+        log_level = logging.DEBUG
+    logger.setLevel(log_level)
+
+    servers = []
+    for server in args.servers:
+        server = re.split("@|:", server[0])
+        if len(server) < 2:
+            logger.error("Need username@hostname given in this format")
+            raise Exception()
+        s = {"username": server[0],
+             "hostname": server[1],
+             "port": 22 if len(server) < 3 else int(server[2])}
+        servers.append(s)
+        logger.info("Connecting to {}".format(s))
+
+    logger.info("pyssher started")
     try:
         agent = Pyssher(servers)
-        asyncio.get_event_loop().run_until_complete(agent.run_command_in_all_clients(server_command))
-    except (OSError, asyncssh.Error) as exc:
+        asyncio.get_event_loop().run_until_complete(agent.run_command_in_all_clients(args.command, args.keypath))
+    except Exception as exc:
         sys.exit('SSH connection failed: ' + str(exc))
+
+if __name__ == '__main__':
+    pyssher()
